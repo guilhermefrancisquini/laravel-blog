157eaceb914475dc1fb9d8b1d97249a4a756c529
Guilherme Francisquini <guilherme.francisquini@hotmail.com>
2015-10-01 20:25:50 -0300

    Ajustando rota

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -6,13 +6,13 @@
 
 @section('content')
     @foreach($posts as $post)
-
+    <!-- blog-post -->
     <div class="blog-post">
         <h2 class="blog-post-title">{{ $post['titulo'] }}</h2>
         <p class="blog-post-meta">{{ $post['data'] }} by <a href="#">Guilherme Francisquini</a></p>
-
         <p>{{ $post['conteudo'] }}</p>
+    </div>
+    <!-- /.blog-post -->
 
-    </div><!-- /.blog-post -->
     @endforeach
 @stop
